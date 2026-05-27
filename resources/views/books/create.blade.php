008847d2554232684541ba12787604e6902ad485
Aleksis Udris <aleksis060806@gmail.com>
2026-05-27 11:37:49 +0300

    idk

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -4,39 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <script type="importmap">
-        {
-        "imports": {
-            "@material/web/": "https://esm.run/@material/web/"
-        }
-        }
-    </script>
-    <script type="module">
-        import '@material/web/all.js';
-        import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
-        import '@material/web/textfield/md-outlined-text-field.js';
-
-        document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
-    </script>
 </head>
 <body>
-    <h1>Create Book</h1>
-    <form action="{{ route('books.store') }}" method="POST">
-        @csrf
-        <div>
-            <label for="title">Title:</label>
-            <md-outlined-text-field id="title" name="title" required></md-outlined-text-field>
-        </div>
-        <div>
-            <label for="ISBN">ISBN:</label>
-            <md-outlined-text-field id="ISBN" name="ISBN" required></md-outlined-text-field>
-        </div>
-        <div>
-            <label for="available_copies">Available Copies:</label>
-            <input type="number" id="available_copies" name="available_copies" required>
-        </div>
-        <button type="submit">Create Book</button>
-    </form>
+    <!-- TITLE -->
+    <div class="top-bar">
+
+    </div>
+    
+    <!-- MAIN CONTENT -->
+    <div class="main-content">
+    </div>
+    
+    <!-- FOOTER -->
+    <div class="footer">
+        
+    </div>
 </body>
 </html>
